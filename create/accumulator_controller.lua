2f53c231019d27e0d4cac4091605969def59afaf
Bernardo Hernandez Hernandez <bhdzhdz@gmail.com>
2024-07-07 17:38:58 -0600

    use native adapters

--- create/accumulator_controller.lua
+++ create/accumulator_controller.lua
@@ -1,21 +1,21 @@
-local speedControler = peripheral.wrap("left");
-local speedometer = peripheral.wrap("bottom");
-local accumulator = peripheral.wrap("back");
+local speedControler = peripheral.wrap("back");
+local speedometer = peripheral.wrap("top");
+local accumulator = peripheral.wrap("right");
 
 
 while true do
   local accumulatorPercent = accumulator.getPercent();
 
   print("Accumulator: " .. accumulatorPercent .. "%");
-  print("Speedometer: " .. speedometer.getKineticSpeed("left") .. "rpm");
+  print("Speedometer: " .. speedometer.getTargetSpeed() .. "rpm");
 
   local targetSpeed = 0;
 
   if accumulatorPercent < 100 then
-    targetSpeed = speedometer.getKineticTopSpeed("left");
+    targetSpeed = speedometer.getTargetSpeed();
   end
 
-  speedControler.setTargetSpeed("back", targetSpeed);
+  speedControler.setTargetSpeed(targetSpeed);
 
   print("Speed set to " .. targetSpeed);
 
